abb513549ffca9d25a097690f7dce7831b646dd8
NikolGaberska6 <ngaberska@tu-sofia.bg>
2025-11-17 12:24:39 +0200

    Update check_leap_year.py

--- UniExercises/17.11/check_leap_year.py
+++ UniExercises/17.11/check_leap_year.py
@@ -1,5 +1,5 @@
-year_days = input("Въведете колко дни има в годината: ")
-if year_days == 366:
+year = int(input("Въведете годината: "))
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print("Годината е високосна!")
 else:
     print("Годината не е високосна!")
